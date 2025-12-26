c3b068a75c4c6ef47f5bb6ac4a3eb65940fe6fe5
bpppoh <ponlawat.changto@gmail.com>
2025-12-26 09:50:06 +0700

    2210 Lab4 Q1

---
+++ 2210/Lab04/6601001123_Lab4_Q1.py
@@ -0,0 +1,26 @@
+def printDeck(deck) :
+    string = ''
+    for i in range(len(deck)) :
+        string += ' ' + deck[i]
+    print(string)
+
+try :
+    deck = input("Please enter the cards in the deck (seperates by spaces): ").split()
+    # print(deck)
+    command = input("Please enter the commands (e.g. CSCS): ")
+    # print(command)
+    for i in range(len(command)) :
+        match command[i].lower() :
+            case 'c' :
+                mid = len(deck) // 2
+                deck = deck[mid:] + deck[:mid]
+                printDeck(deck)
+            case 's' :
+                tempDeck = []
+                for i in range(len(deck)//2) :
+                    tempDeck.append(deck[i])
+                    tempDeck.append(deck[ (len(deck)//2) + i])
+                deck = tempDeck
+                printDeck(deck)
+except:
+    print("Error")
