43547421e08821666f41f7f715ae3b4009b4fd7e
bpppoh <ponlawat.changto@gmail.com>
2026-02-18 11:51:08 +0700

    3401 fix assignment1_1

--- 3401/assignment 1/6601001123_assignment1_1.py
+++ 3401/assignment 1/6601001123_assignment1_1.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
-from image_process import powerGamma,calculate_hist,image_show
+from image_process import powerGamma,calculate_hist,image_show,equalization
 
 img = cv2.imread('pic1.png', 1)
 cv2.imwrite("./assignment1_1/original_img.jpg",img)
 
 row , col , channel = img.shape
-a = [0.114 , 0.587 , 0.299]
 color_img = np.zeros_like(img,dtype='uint8')
 for space in range(channel) :
     color_img[:,:,space] = powerGamma(img[:,:,space],0.4)
+    # color_img[:,:,space] = equalization(img[:,:,space])
 
 cv2.imwrite("./assignment1_1/color_after_powergamma.jpg",color_img)
 hconcat_img = cv2.hconcat([img,color_img])
