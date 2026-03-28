4b3178394a6020b58646cfb59672263911ccfd7e
bpppoh <ponlawat.changto@gmail.com>
2026-03-28 14:09:08 +0700

    3401 update

--- 3401/assignment_2/review.py
+++ 3401/assignment_2/review.py
@@ -6,8 +6,8 @@ cen = [img.shape[0]/2 , img.shape[1]/2]
 
 T = init_transform()
 T = matrix_translate(T, cen[1], cen[0])
-T = matrix_scaling(T, 0.5, 0.5)
-T = matrix_rotate(T,45)
+# T = matrix_scaling(T, 0.5, 0.5)
+# T = matrix_rotate(T,45)
 T = matrix_scaling(T, -1, 1)
 T = matrix_translate(T, -cen[1], -cen[0])
 newimg = img_transform(img,T)
